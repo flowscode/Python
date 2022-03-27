473ec369e2d5d983465eb809f9af667eedfe59a5
flowscode <joshuadawkins95@gmail.com>
2022-03-27 07:53:34 +0100

    added get by id and post mappings

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,22 @@
+from random import randrange
 from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import Response
+from post import Post
 
 app = FastAPI()
+my_posts = [
+            {"title": "title of post 1",
+            "content": "content of post 1",
+            "id": 1},
+            {"title": "fave car",
+             "content": "BMW",
+             "id": 2}
+        ]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 
 @app.get("/")
@@ -9,13 +24,30 @@ def root():
     return {"message": "Hello World yoooooooo"}
 
 
-@app.get("/allposts")
+@app.get("/posts")
 def get_posts():
-    return [{"data": "this is my first post"}, {"data": "this is my second post"}]
+    return [{"Posts": my_posts}]
 
 
-@app.post("/post")
-def create_post(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"Title: {payLoad['title']} "
-                        f"Content: {payLoad['content']}"}
+@app.post("/posts")
+def create_post(post: Post):
+    if my_posts[0] == "PLACEHOLDER":
+        my_posts.remove("PLACEHOLDER")
+    post.id = randrange(0, 10000000)
+    post_dict = post.dict()
+    my_posts.append(post_dict)
+    return {"Post": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    print(post)
+    if not post:
+        response.status_code = 404
+        response.body = f"Post with id {id} was not found"
+        return {"Response": [
+            {"Status Code": response.status_code},
+            {"Body": response.body},
+        ]}
+    return {"Post": post}
--- FastAPI/post.py
+++ FastAPI/post.py
@@ -1,2 +1,10 @@
-class post:
-    
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    id: Optional[int] = None
+    title: str
+    content: str
+    rating: Optional[int] = None
+
